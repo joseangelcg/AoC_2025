827d68c7268a174881a4942ba4a999f1652335bc
Jose Contreras (Potter) <jose.contreras.godoy@gmail.com>
2025-12-01 23:39:05 -0600

    Ran go fmt for god's sake

--- day1/main.go
+++ day1/main.go
@@ -8,44 +8,50 @@ import (
 	// "strings"
 )
 
-func processRotation(curPos int ,rotation string) (int,int){
+func processRotation(curPos int, rotation string) (int, int) {
 	//Get direction of rotation
 	s := rotation[0]
 	var newPos int
 
 	//Get how far we will rotate from curPos, %100 to avoid multiple turns
-	turn,_ := strconv.Atoi(rotation[1:])
-	overs := turn/100
-	turn = turn%100
+	turn, _ := strconv.Atoi(rotation[1:])
+	overs := turn / 100
+	turn = turn % 100
 
-	if (s == 'L') {
+	if s == 'L' {
 
 		//we will substract from curPos
 		newPos = curPos - turn
 		//if we are negative, then we overflowed. add 100
-		if (newPos<0) {
+		if newPos < 0 {
 			newPos += 100
 
 			//only add if we didnt start nor land on 0.
-			if (curPos !=0 && newPos != 0) {overs++}
+			if curPos != 0 && newPos != 0 {
+				overs++
+			}
 		}
 
-	} else if (s == 'R'){
+	} else if s == 'R' {
 
 		//we will substract from curPos
 		newPos = curPos + turn
 		//if we are more than 99, then we overflowed. substract 100
-		if(newPos>99) {
+		if newPos > 99 {
 			newPos -= 100
 			//only add if we didnt start nor land on 0.
-			if (curPos !=0 && newPos != 0) {overs++}
+			if curPos != 0 && newPos != 0 {
+				overs++
+			}
 		}
 	}
 
 	//landed on 0. increment over count here
-	if newPos==0 {overs++}
+	if newPos == 0 {
+		overs++
+	}
 
-	fmt.Printf("Old Position %d moves: %s to position %d, overs: %d\n", curPos, rotation, newPos,overs)
+	fmt.Printf("Old Position %d moves: %s to position %d, overs: %d\n", curPos, rotation, newPos, overs)
 
 	return newPos, overs
 }
@@ -54,16 +60,16 @@ func main() {
 	var dialPos int = 50
 	var password int = 0
 
-	f,err := os.Open("directions.txt")
-	if(err != nil){
+	f, err := os.Open("directions.txt")
+	if err != nil {
 		fmt.Println("Error file does not open")
 		return
 	}
 	//Scan directions
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
-		var overs int=0;
-		dialPos,overs = processRotation(dialPos, scanner.Text())
+		var overs int = 0
+		dialPos, overs = processRotation(dialPos, scanner.Text())
 		//Add number of times we clicked over 0 or landed
 		password += overs
 	}
