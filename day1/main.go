2b4c84f5d06eed4327dbab3a7d6a942710aca8a5
Jose Contreras (Potter) <jose.contreras.godoy@gmail.com>
2025-12-01 23:38:37 -0600

    Solution part 2 day 1

--- day1/main.go
+++ day1/main.go
@@ -8,13 +8,14 @@ import (
 	// "strings"
 )
 
-func processRotation(curPos int ,rotation string) int{
+func processRotation(curPos int ,rotation string) (int,int){
 	//Get direction of rotation
 	s := rotation[0]
 	var newPos int
 
 	//Get how far we will rotate from curPos, %100 to avoid multiple turns
 	turn,_ := strconv.Atoi(rotation[1:])
+	overs := turn/100
 	turn = turn%100
 
 	if (s == 'L') {
@@ -22,18 +23,31 @@ func processRotation(curPos int ,rotation string) int{
 		//we will substract from curPos
 		newPos = curPos - turn
 		//if we are negative, then we overflowed. add 100
-		if(newPos<0) {newPos += 100}
+		if (newPos<0) {
+			newPos += 100
+
+			//only add if we didnt start nor land on 0.
+			if (curPos !=0 && newPos != 0) {overs++}
+		}
 
 	} else if (s == 'R'){
 
 		//we will substract from curPos
 		newPos = curPos + turn
 		//if we are more than 99, then we overflowed. substract 100
-		if(newPos>99) {newPos -= 100}
-
+		if(newPos>99) {
+			newPos -= 100
+			//only add if we didnt start nor land on 0.
+			if (curPos !=0 && newPos != 0) {overs++}
+		}
 	}
 
-	return newPos
+	//landed on 0. increment over count here
+	if newPos==0 {overs++}
+
+	fmt.Printf("Old Position %d moves: %s to position %d, overs: %d\n", curPos, rotation, newPos,overs)
+
+	return newPos, overs
 }
 
 func main() {
@@ -48,9 +62,10 @@ func main() {
 	//Scan directions
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
-		dialPos = processRotation(dialPos, scanner.Text())
-		// fmt.Printf("New pos is: %d\n", dialPos)
-		if(dialPos==0) {password++}
+		var overs int=0;
+		dialPos,overs = processRotation(dialPos, scanner.Text())
+		//Add number of times we clicked over 0 or landed
+		password += overs
 	}
 
 	fmt.Printf("The password is: %d\n", password)
