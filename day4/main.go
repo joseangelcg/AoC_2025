721f9871024c5167fe24ed6ba30b4f66973168aa
Jose Contreras (Potter) <jose.contreras.godoy@gmail.com>
2025-12-05 10:03:18 -0600

    small tweaks to day4

--- day4/main.go
+++ day4/main.go
@@ -8,24 +8,32 @@ import (
 
 const neighborhoodSize int = 1
 
-func isAccessible(ptr [][]rune, row int, col int, limit int) bool{
+func isAccessible(ptr [][]rune, row int, col int, limit int) bool {
 
-	left  := col - neighborhoodSize
+	left := col - neighborhoodSize
 	right := col + neighborhoodSize
-	up    := row - neighborhoodSize
-	down  := row + neighborhoodSize
+	up := row - neighborhoodSize
+	down := row + neighborhoodSize
 
-	if left < 0 { left = 0 }
-	if right >= len(ptr[0]) { right = len(ptr[0])-1 }
+	if left < 0 {
+		left = 0
+	}
+	if right >= len(ptr[0]) {
+		right = len(ptr[0]) - 1
+	}
 
-	if up < 0 { up = 0 }
-	if down >= len(ptr) { down = len(ptr)-1 }
+	if up < 0 {
+		up = 0
+	}
+	if down >= len(ptr) {
+		down = len(ptr) - 1
+	}
 
 	count := 0
-	for i:=up; i<=down; i++{
-		for j:= left; j<=right; j++{
-			if !(i==row && j==col) &&
-			(ptr[i][j] == '@' || ptr[i][j] == 'x'){
+	for i := up; i <= down; i++ {
+		for j := left; j <= right; j++ {
+			if !(i == row && j == col) &&
+				(ptr[i][j] == '@' || ptr[i][j] == 'x') {
 				count++
 				if count >= limit {
 					return false
@@ -48,11 +56,11 @@ func parseFile(file string) [][]rune {
 
 	var matrix [][]rune
 
-	for scanner.Scan(){
+	for scanner.Scan() {
 		var row []rune
 		line := scanner.Text()
-		for _,c := range line{
-			row = append(row,c)
+		for _, c := range line {
+			row = append(row, c)
 		}
 		matrix = append(matrix, row)
 	}
@@ -61,37 +69,38 @@ func parseFile(file string) [][]rune {
 
 }
 
-func scanRolls(rolls [][]rune, mark bool) int{
+func scanRolls(rolls [][]rune, mark bool) int {
 	count := 0
-	for i:=0; i<len(rolls);i++ {
-		for j:=0; j<len(rolls[i]); j++ {
-			if rolls[i][j] == '@' && isAccessible(rolls,i,j,4){
+	for i := 0; i < len(rolls); i++ {
+		for j := 0; j < len(rolls[i]); j++ {
+			if rolls[i][j] == '@' && isAccessible(rolls, i, j, 4) {
 				count++
-				if mark { rolls[i][j] = 'x'}
+				if mark {
+					rolls[i][j] = 'x'
+				}
 			}
 		}
 	}
 	return count
 }
 
-func scanRolls2(rolls [][]rune) int{
+func scanRolls2(rolls [][]rune) int {
 
-	count_prev := 0
 	count := 0
 	for {
-		count += scanRolls(rolls, true)
-		if count == count_prev {
+		new_count := scanRolls(rolls, true)
+		if new_count == 0 {
 			break
 		}
-		count_prev = count
+		count += new_count
 		removeRolls(rolls)
 	}
 	return count
 }
 
-func removeRolls(rolls [][]rune){
-	for i:=0; i<len(rolls);i++ {
-		for j:=0; j<len(rolls[i]); j++ {
+func removeRolls(rolls [][]rune) {
+	for i := 0; i < len(rolls); i++ {
+		for j := 0; j < len(rolls[i]); j++ {
 			if rolls[i][j] == 'x' {
 				rolls[i][j] = '.'
 			}
@@ -99,14 +108,14 @@ func removeRolls(rolls [][]rune){
 	}
 }
 
-func main()  {
-	m := parseFile("input.txt")
+func main() {
+	m := parseFile(os.Args[1])
 
 	//part 1 do it once
-	count := scanRolls(m,false)
-	fmt.Printf("Part1: %d rolls are accessible\n",count)
+	count := scanRolls(m, false)
+	fmt.Printf("Part1: %d rolls are accessible\n", count)
 
 	// part 2: do it until no change in count
 	count = scanRolls2(m)
-	fmt.Printf("Part2: %d rolls are accessible\n",count)
+	fmt.Printf("Part2: %d rolls are accessible\n", count)
 }
